32307162b992cc9a26c7fa7812a86b20c63db800
ChoiHyeongBin <chekqhksha@gmail.com>
2023-03-31 22:09:33 +0900

    알고리즘 풀이 첫커밋

---
+++ Algorithm/src/codingTest/ProgrammersCodingTest.java
@@ -0,0 +1,45 @@
+package codingTest;
+
+
+public class ProgrammersCodingTest {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 2;
+		int n = 20;
+		
+		solution(a, b, n);
+	}
+
+	static int answer = 0;
+	public static int solution(int a, int b, int n) {
+        for (int i = n; i > 0; i--) {
+        	if (i % a == 0) {
+        		System.out.println("i : " + i);
+        		n = n - i + (i / a) * b;
+        		System.out.println("n : " + n);
+        		System.out.println("a : " + a);
+        		System.out.println("받은것 : " + (i / a) * b);
+        		answer += (i / a) * b;
+        		System.out.println("answer11 : " + answer);
+        		System.out.println();
+        		
+        		// 남아있는 개수가 콜라를 받기 위해 마트에 주어야 하는 병 수보다 적으면 멈춤
+        		if (n < a) {
+        			System.out.println("if문 탔음");
+        			System.out.println("최종 answer : " + answer);
+        			return answer;
+        		} else {
+        			System.out.println("else문 탔음");
+        			solution(a, b, n);
+        		}
+        		
+        		break;
+        	}
+        }
+        
+        System.out.println("answer22 : " + answer);
+        return answer;
+    }
+	
+}
